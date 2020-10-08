91bcdd60780ce679d3f0cdd1910e5da4c044f956
Aironas <6zouchee9@gmail.com>
2020-10-08 14:28:06 +0200

    Initial COmmit

---
+++ slump/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace slump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Knives and Reeboks");
+
+            Random generator = new Random();
+
+            bool alive = true;
+
+            Console.WriteLine("Welcome to Knives and Reeboks");
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+
+
+
+
+            while (alive)
+            {
+
+
+                int tal = generator.Next(100);
+
+                Console.WriteLine(tal % 10);
+
+                Console.WriteLine(tal % 100);
+
+
+
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
